0e0de4b59a3cffea32fb4ea14bc1c4babebe840d
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-03-22 10:44:17 +0500

    enable & disable

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         {{ MainButton }}
         <Btn @click="onShowHide(!MainButton.show)"> {{ MainButton.show ? 'Hide Main Button' : 'Show Main Button' }} </Btn>
         <Btn @click="onSetProgress(!MainButton.progress)"> {{ MainButton.progress ? 'Hide Main Button Progress' : 'Show Main Button Progress' }} </Btn>
-        <Btn @click="onDisable(!MainButton.disable)"> {{ MainButton.disable ? 'Hide Main Button Progress' : 'Show Main Button Progress' }} </Btn>
+        <Btn @click="onDisable(!MainButton.disable)"> {{ MainButton.disable ? 'Disable Main' : 'Enable Main Button' }} </Btn>
     </div>
 </template>
 
